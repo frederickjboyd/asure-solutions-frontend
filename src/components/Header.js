c4cce27e467ebae5a5f68f300a7b1e2a8cbc664a
frederickjboyd <frederickjboyd@gmail.com>
2018-08-20 17:20:15 -0400

    Removed default layout and added a preliminary Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="container">
+        <p className="nav">home products about us concat</p>
+      </div>
+    );
+  }
+}
