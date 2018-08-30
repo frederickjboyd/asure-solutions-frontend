1b7f319a88b65f901d89056c3e86e065f99e2944
Frederick Boyd <30788043+frederickjboyd@users.noreply.github.com>
2018-08-30 00:37:32 -0400

    Completed layout of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './Header.css';
+import logo from '../logo.png';
 
 export default class Header extends Component {
   render() {
     return (
       <div className="container">
-        <p className="nav">home products about us concat</p>
+        <img src={logo} alt="logo" className="logo" />
+        <div>
+          <a className="nav">home</a>
+          <a className="nav">products</a>
+          <a className="nav">about us</a>
+          <a className="nav">contact</a>
+        </div>
       </div>
     );
   }
