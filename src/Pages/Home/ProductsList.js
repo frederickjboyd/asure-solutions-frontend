a123c5a5f74d7eda5a9d494aa06e6a0caefc5359
Frederick Boyd <30788043+frederickjboyd@users.noreply.github.com>
2018-09-13 12:00:59 -0400

    Added background transition to product image on home page

--- src/Pages/Home/ProductsList.js
+++ src/Pages/Home/ProductsList.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import './ProductsList.css';
+import { Link } from 'react-router-dom';
+import LTD from '../../assets/products/LTD.png';
 
 export default class ProductsList extends Component {
   render() {
     return (
       <div className="listContainer">
         <h2>Quick Draw Light and Timing Device</h2>
+        <div className="imageContainer">
+          <Link to="/products">
+            <img
+              src={LTD}
+              alt="Light and Timing Device"
+              className="productImage"
+            />
+          </Link>
+        </div>
       </div>
     );
   }
--- src/Pages/Home/Slideshow.js
+++ src/Pages/Home/Slideshow.js
@@ -7,6 +7,17 @@ export default class Slideshow extends Component {
     return (
       <div className="slideshowContainer">
         <img src={slideshow1} alt="slideshow1" className="slideshow" />
+        <div className="imageText">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
       </div>
     );
   }
