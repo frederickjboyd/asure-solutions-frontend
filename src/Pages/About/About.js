5f99f2e9741f6c41997bb086b9333e228ed0e73d
Frederick Boyd <30788043+frederickjboyd@users.noreply.github.com>
2018-09-14 01:11:24 -0400

    Finished about page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import './About.css';
+import GroupPhoto from '../../assets/group-photo.png';
 
 export default class About extends Component {
   render() {
     return (
-      <div>
-        <p>About us!</p>
+      <div className="aboutContainer">
+        <div className="groupPhotoContainer">
+          <img src={GroupPhoto} alt="Group" className="groupPhoto" />
+        </div>
+        <div className="aboutDescriptionContainer">
+          <h2>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </h2>
+        </div>
       </div>
     );
   }
