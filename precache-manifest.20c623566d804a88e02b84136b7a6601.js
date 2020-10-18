be48bc464f73d03ed8503671a41d58bf7da681fa
Frederick Boyd <frederickjboyd@gmail.com>
2020-10-18 15:18:21 -0400

    Updates

---
+++ precache-manifest.20c623566d804a88e02b84136b7a6601.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54d826594a79c43c9657777867ddd55",
+    "url": "/asure-solutions-frontend/index.html"
+  },
+  {
+    "revision": "1ee1a510a5cf465b9441",
+    "url": "/asure-solutions-frontend/static/css/main.a000e6e0.chunk.css"
+  },
+  {
+    "revision": "4e2a367bd33c23bd90e2",
+    "url": "/asure-solutions-frontend/static/js/2.b869df24.chunk.js"
+  },
+  {
+    "revision": "1ee1a510a5cf465b9441",
+    "url": "/asure-solutions-frontend/static/js/main.9ccb9eaa.chunk.js"
+  },
+  {
+    "revision": "0291283873f13933d767",
+    "url": "/asure-solutions-frontend/static/js/runtime~main.f31d0e9e.js"
+  },
+  {
+    "revision": "85547eb479ce6885925b6d2673ff4c87",
+    "url": "/asure-solutions-frontend/static/media/LTD.85547eb4.png"
+  },
+  {
+    "revision": "70dac5b9af046943002e544e0064cfcf",
+    "url": "/asure-solutions-frontend/static/media/clock-line.70dac5b9.svg"
+  },
+  {
+    "revision": "df1b3594a5d318663b961585c11d8dc4",
+    "url": "/asure-solutions-frontend/static/media/firewall-line.df1b3594.svg"
+  },
+  {
+    "revision": "ff1930c689d91f1c9bd61593fcc93be1",
+    "url": "/asure-solutions-frontend/static/media/group-photo.ff1930c6.png"
+  },
+  {
+    "revision": "ebd7826e643bf322d75f8d784f9238b9",
+    "url": "/asure-solutions-frontend/static/media/lightbulb-line.ebd7826e.svg"
+  },
+  {
+    "revision": "9dbb92a416cf62be8b554ff1f0f1b74c",
+    "url": "/asure-solutions-frontend/static/media/logo.9dbb92a4.png"
+  },
+  {
+    "revision": "532053d6a280bf373f82d35e0be49356",
+    "url": "/asure-solutions-frontend/static/media/shopping-bag-line.532053d6.svg"
+  },
+  {
+    "revision": "e0e3ee1e17a40bf9454e6e00d6d7aa53",
+    "url": "/asure-solutions-frontend/static/media/slideshow1.e0e3ee1e.png"
+  }
+]);
