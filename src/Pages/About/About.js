104dfb31e1a4b97c49027c4f178fc3535cac0647
Frederick Boyd <frederickjboyd@gmail.com>
2020-10-18 16:22:33 -0400

    Update info

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -11,7 +11,7 @@ export default class About extends Component {
         </div>
         <div className="aboutDescriptionContainer">
           <h2>
-            We are a group of second year engineering students from the
+            We are a group of third year engineering students from the
             University of Toronto, with the ultimate goal of making a positive
             and meaningful impact on the community. With the key focus on
             accessibility, our team will collaborate with you to explore
